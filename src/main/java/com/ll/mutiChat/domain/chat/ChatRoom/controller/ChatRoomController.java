3422b178575a9f0adfa01079b16d0e55e1810aa1
cnxw4570123 <cnxw4570123@gmail.com>
2025-01-02 14:57:21 +0900

    feat : 채팅방 생성 기능 추가

--- src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/controller/ChatRoomController.java
+++ src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/controller/ChatRoomController.java
@@ -1,7 +1,13 @@
 package com.ll.mutiChat.domain.chat.ChatRoom.controller;
 
+import java.util.Map;
+
+import com.ll.mutiChat.domain.chat.ChatRoom.entity.ChatRoom;
 import com.ll.mutiChat.domain.chat.ChatRoom.service.ChatRoomService;
+import com.ll.mutiChat.global.rsData.RsData;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,18 +15,25 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/chat/room")
 public class ChatRoomController {
-    private final ChatRoomService chatRoomService;
+	private final ChatRoomService chatRoomService;
 
-    @GetMapping("/{roomId}")
-    @ResponseBody
-    public String showRoom(@PathVariable long roomId, @RequestParam(defaultValue = "NoName") String writerName) {
+	@GetMapping("/{roomId}")
+	@ResponseBody
+	public String showRoom(@PathVariable long roomId, @RequestParam(defaultValue = "NoName") String writerName) {
 
-        return String.format("%d 번 채팅방 입니다. writer : %s", roomId, writerName);
-    }
+		return String.format("%d 번 채팅방 입니다. writer : %s", roomId, writerName);
+	}
 
-    @GetMapping("/make")
-    public String makeRoom() {
-        return "domain/chat/chatRoom/make";
-    }
+	@GetMapping("/make")
+	public String makeRoom() {
+		return "domain/chat/chatRoom/make";
+	}
+
+	@PostMapping("/make")
+	public RsData<ChatRoom> makeRoom(@RequestBody Map<String, String> roomInfo) {
+		String name = roomInfo.getOrDefault("roomInfo", "NoName");
+		ChatRoom chatRoom = chatRoomService.saveRoom(name);
+		return RsData.of("200", "채팅방 생성 완료", chatRoom);
+	}
 
 }
--- src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/service/ChatRoomService.java
+++ src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/service/ChatRoomService.java
@@ -1,11 +1,20 @@
 package com.ll.mutiChat.domain.chat.ChatRoom.service;
 
+import com.ll.mutiChat.domain.chat.ChatRoom.entity.ChatRoom;
 import com.ll.mutiChat.domain.chat.ChatRoom.repository.ChatRoomRepository;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRoomService {
-    private final ChatRoomRepository chatRoomRepository;
+	private final ChatRoomRepository chatRoomRepository;
+
+	public ChatRoom saveRoom(String name) {
+		return chatRoomRepository.save(ChatRoom.builder()
+			.name(name)
+			.build());
+	}
 }

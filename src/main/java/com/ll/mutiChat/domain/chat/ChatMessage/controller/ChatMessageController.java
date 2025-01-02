408aace289df787c5de7227679f2ac7304abefd2
Ojiner <dbstjdrud115@gmail.com>
2025-01-02 14:54:55 +0900

    branch

--- src/main/java/com/ll/mutiChat/domain/chat/ChatMessage/controller/ChatMessageController.java
+++ src/main/java/com/ll/mutiChat/domain/chat/ChatMessage/controller/ChatMessageController.java
@@ -7,5 +7,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 @RequiredArgsConstructor
 public class ChatMessageController {
+
     private final ChatMessageService chatMessageService;
+
 }

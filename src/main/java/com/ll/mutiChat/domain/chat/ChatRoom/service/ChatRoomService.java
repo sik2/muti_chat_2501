b7940e3d1f5b26f8e74e7416c1a0b7f3bd574872
daehyuk1231 <bangdaehyeog@bangdaehyeog-ui-MacBookAir.local>
2025-01-02 17:11:11 +0900

    polling

--- src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/service/ChatRoomService.java
+++ src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/service/ChatRoomService.java
@@ -1,11 +1,41 @@
 package com.ll.mutiChat.domain.chat.ChatRoom.service;
 
+import com.ll.mutiChat.domain.chat.ChatMessage.entity.ChatMessage;
+import com.ll.mutiChat.domain.chat.ChatRoom.entity.ChatRoom;
 import com.ll.mutiChat.domain.chat.ChatRoom.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
+
+    @Transactional // 트랜잭션 지원, 작업 실패 시 롤백 보장
+    public ChatRoom make(String name) {
+        ChatRoom chatRoom = ChatRoom.builder()
+                .name(name)
+                .build();
+
+        chatRoomRepository.save(chatRoom);
+        return chatRoom;
+    }
+
+    @Transactional
+    public ChatMessage write(long roomId, String writerName, String content) {
+        ChatRoom chatRoom = chatRoomRepository.findById(roomId).get();
+        return chatRoom.writeMessage(writerName, content);
+    }
+
+    public Optional<ChatRoom> findById(long roomId) {
+        return chatRoomRepository.findById(roomId);
+    }
+
+    public List<ChatRoom> findAll() {
+        return chatRoomRepository.findAll();
+    }
 }

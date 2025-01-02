970b4e988afa3ed23af63bb38a45f50b187fd10f
kknaks <benesia93@naver.com>
2025-01-02 16:14:00 +0900

    .

--- src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/controller/ChatRoomController.java
+++ src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/controller/ChatRoomController.java
@@ -1,9 +1,13 @@
 package com.ll.mutiChat.domain.chat.ChatRoom.controller;
 
+import com.ll.mutiChat.domain.chat.ChatRoom.entity.ChatRoom;
 import com.ll.mutiChat.domain.chat.ChatRoom.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,16 +15,29 @@ import org.springframework.web.bind.annotation.*;
 public class ChatRoomController {
     private final ChatRoomService chatRoomService;
 
-    @GetMapping("/{roomId}")
+    @GetMapping("list")
+    public String list(Model model){
+        model.addAttribute("chatRooms",chatRoomService.findAll());
+        System.out.println(chatRoomService.findAll());
+        return "domain/chat/chatRoom/list";
+    }
+
+    @GetMapping("{roomId}")
     @ResponseBody
     public String showRoom(@PathVariable long roomId, @RequestParam(defaultValue = "NoName") String writerName) {
 
         return String.format("%d 번 채팅방 입니다. writer : %s", roomId, writerName);
     }
 
-    @GetMapping("/make")
-    public String makeRoom() {
+    @GetMapping("make")
+    public String makeRoomForm() {
         return "domain/chat/chatRoom/make";
+    }
+
+    @PostMapping("make")
+    public String makeRoom(@RequestParam String name){
+        chatRoomService.makeChatRoom(name);
+        return "redirect:list";
     }
 
 }
--- src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/service/ChatRoomService.java
+++ src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/service/ChatRoomService.java
@@ -1,11 +1,24 @@
 package com.ll.mutiChat.domain.chat.ChatRoom.service;
 
+import com.ll.mutiChat.domain.chat.ChatRoom.entity.ChatRoom;
 import com.ll.mutiChat.domain.chat.ChatRoom.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
+
+    public List<ChatRoom> findAll(){
+        return chatRoomRepository.findAll();
+    }
+
+    public ChatRoom makeChatRoom(String name){
+        return chatRoomRepository.save(ChatRoom.builder()
+            .name(name)
+            .build());
+    }
 }

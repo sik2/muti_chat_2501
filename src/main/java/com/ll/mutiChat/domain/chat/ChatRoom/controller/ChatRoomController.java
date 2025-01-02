b0c78975391e95bd2a7bbbf119ca6bbfdeb2683c
ksuju <139477852+ksuju@users.noreply.github.com>
2025-01-02 16:03:29 +0900

    feat:채팅방 생성

--- src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/controller/ChatRoomController.java
+++ src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/controller/ChatRoomController.java
@@ -1,26 +1,68 @@
 package com.ll.mutiChat.domain.chat.ChatRoom.controller;
 
+import com.ll.mutiChat.domain.chat.ChatRoom.entity.ChatRoom;
 import com.ll.mutiChat.domain.chat.ChatRoom.service.ChatRoomService;
+import com.ll.mutiChat.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/chat/room")
 public class ChatRoomController {
+
+    Logger logger = LoggerFactory.getLogger(ChatRoomController.class);
     private final ChatRoomService chatRoomService;
 
-    @GetMapping("/{roomId}")
-    @ResponseBody
-    public String showRoom(@PathVariable long roomId, @RequestParam(defaultValue = "NoName") String writerName) {
+    // 채팅방 목록 화면 이동
+    @GetMapping("/list")
+    public String chatRoomList(Model model) {
+        List<ChatRoom> chatRooms = new ArrayList<>();
+        // 모든 채팅방 목록 출력
+        chatRooms = chatRoomService.showRoomList();
 
-        return String.format("%d 번 채팅방 입니다. writer : %s", roomId, writerName);
+        model.addAttribute("chatRooms", chatRooms);
+        return "domain/chat/chatRoom/list";
     }
 
+    // 채팅방 입장 화면 이동
+    @GetMapping("/{roomId}")
+    public String showRoom(@PathVariable long roomId,
+                           @RequestParam(defaultValue = "NoName") String chatRoomName,
+                           Model model) {
+
+        model.addAttribute("roomId", roomId);
+        model.addAttribute("chatRoomName", chatRoomName);
+
+        return "domain/chat/chatRoom/room";
+    }
+
+    // 채팅방 생성 화면 이동
     @GetMapping("/make")
     public String makeRoom() {
         return "domain/chat/chatRoom/make";
+    }
+
+    @PostMapping("/make")
+    public String makeRoom(@RequestParam String name) {
+
+        try {
+            // 채팅방 생성
+            chatRoomService.makeRoom(name);
+            logger.info("채팅방 생성 완료");
+            return "redirect:/chat/room/list";
+        } catch (Exception e) {
+            logger.error("채팅방 생성 실패", e);
+            return "redirect:/chat/room/list";
+        }
+
     }
 
 }
---
+++ src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/dto/ChatRoomDto.java
@@ -0,0 +1,19 @@
+package com.ll.mutiChat.domain.chat.ChatRoom.dto;
+
+import com.ll.mutiChat.domain.chat.ChatRoom.entity.ChatRoom;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * packageName    : com.ll.mutiChat.domain.chat.ChatRoom.dto
+ * fileName       : ChatRoomDto
+ * author         : sungjun
+ * date           : 2025-01-02
+ * description    : 자동 주석 생성
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-01-02        kyd54       최초 생성
+ */
+public record ChatRoomDto (ChatRoom chatRoom){}

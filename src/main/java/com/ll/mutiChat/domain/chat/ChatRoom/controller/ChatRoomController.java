b3ced5114446944759516ad889026a14a2958058
whqtker <chosh2001@naver.com>
2025-01-02 15:56:48 +0900

    feat: POST make 구현

--- src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/controller/ChatRoomController.java
+++ src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/controller/ChatRoomController.java
@@ -1,9 +1,13 @@
 package com.ll.mutiChat.domain.chat.ChatRoom.controller;
 
+import com.ll.mutiChat.domain.chat.ChatRoom.entity.ChatRoom;
 import com.ll.mutiChat.domain.chat.ChatRoom.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -11,11 +15,11 @@ import org.springframework.web.bind.annotation.*;
 public class ChatRoomController {
     private final ChatRoomService chatRoomService;
 
-    @GetMapping("/{roomId}")
-    @ResponseBody
-    public String showRoom(@PathVariable long roomId, @RequestParam(defaultValue = "NoName") String writerName) {
-
-        return String.format("%d 번 채팅방 입니다. writer : %s", roomId, writerName);
+    @GetMapping("/list")
+    public String listRoom(Model model) {
+        List<ChatRoom> chatRooms = chatRoomService.findAll();
+        model.addAttribute("chatRooms", chatRooms);
+        return "domain/chat/chatRoom/list";
     }
 
     @GetMapping("/make")
@@ -23,4 +27,19 @@ public class ChatRoomController {
         return "domain/chat/chatRoom/make";
     }
 
+    @PostMapping("/make")
+    public String makeRoom(@RequestParam String roomName) {
+        chatRoomService.makeRoom(roomName);
+        return "redirect:/chat/room/list";
+    }
+
+    @GetMapping("/{roomId}")
+    @ResponseBody
+    public String showRoom(@PathVariable long roomId, @RequestParam(defaultValue = "NoName") String writerName) {
+        return String.format("%d 번 채팅방 입니다. writer : %s", roomId, writerName);
+    }
+
+    //@PostMapping("/{roomId}/make")
+
+    //@GetMapping("/{roomId}/messageAfter/{AfterId}")
 }

300bc30486311dad7a55f40dcbcb4701ee6d5efa
황민지 <hminji4565@gmail.com>
2025-01-02 16:15:13 +0900

    ChatRoom 진행 중

--- src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/repository/ChatRoomRepository.java
+++ src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/repository/ChatRoomRepository.java
@@ -3,5 +3,11 @@ package com.ll.mutiChat.domain.chat.ChatRoom.repository;
 import com.ll.mutiChat.domain.chat.ChatRoom.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    void save(String name);
+
+    List<ChatRoom> findAll();
 }
--- src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/service/ChatRoomService.java
+++ src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/service/ChatRoomService.java
@@ -1,11 +1,22 @@
 package com.ll.mutiChat.domain.chat.ChatRoom.service;
 
+import com.ll.mutiChat.domain.chat.ChatRoom.entity.ChatRoom;
 import com.ll.mutiChat.domain.chat.ChatRoom.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRoomService {
     private final ChatRoomRepository chatRoomRepository;
+
+    public void makeRoom(String name) {
+        chatRoomRepository.save(ChatRoom.builder().name(name).build());
+    }
+
+    public List<ChatRoom> getRooms() {
+        return chatRoomRepository.findAll();
+    }
 }

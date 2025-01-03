bd2f0818b8adac756e6de56b8eb9153f3faab349
junghyun13 <107833502+junghyun13@users.noreply.github.com>
2025-01-03 10:50:21 +0900

    Update ChatRoomController.java

--- src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/controller/ChatRoomController.java
+++ src/main/java/com/ll/mutiChat/domain/chat/ChatRoom/controller/ChatRoomController.java
@@ -1,10 +1,14 @@
 package com.ll.mutiChat.domain.chat.ChatRoom.controller;
 
-import com.ll.mutiChat.domain.chat.ChatRoom.dto.ChatRoomDTO;
+import com.ll.mutiChat.domain.chat.ChatMessage.entity.ChatMessage;
+import com.ll.mutiChat.domain.chat.ChatMessage.service.ChatMessageService;
 import com.ll.mutiChat.domain.chat.ChatRoom.entity.ChatRoom;
 import com.ll.mutiChat.domain.chat.ChatRoom.service.ChatRoomService;
+import com.ll.mutiChat.global.rsData.RsData;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,62 +16,84 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequiredArgsConstructor
-@Slf4j
 @RequestMapping("/chat/room")
+@RequiredArgsConstructor
 public class ChatRoomController {
-
     private final ChatRoomService chatRoomService;
+    private final ChatMessageService chatMessageService;
 
-    //채팅방 목록조회
-    @GetMapping("list")
-    public String getRoomList(Model model) {
-        List<ChatRoom> chatRooms = chatRoomService.findAll();
-        model.addAttribute("chatRooms", chatRooms);
-        return "domain/chat/chatRoom/list";
+    @GetMapping("/{roomId}")
+    public String showRoom(
+            @PathVariable final long roomId,
+            final String writerName,
+            Model model
+    ) {
+        ChatRoom room = chatRoomService.findById(roomId).get();
+        model.addAttribute("room", room);
+
+        return "domain/chat/chatRoom/room";
     }
 
-    //채팅방 생성화면 이동
     @GetMapping("/make")
-    public String makeRoom() {
+    public String showMake() {
         return "domain/chat/chatRoom/make";
     }
 
-
     @PostMapping("/make")
+    public String make(
+            final String name
+    ) {
+        chatRoomService.make(name);
+
+        return "redirect:/chat/room/list";
+    }
+
+    @GetMapping("/list")
+    public String showList(Model model) {
+        List<ChatRoom> chatRooms = chatRoomService.findAll();
+        model.addAttribute("chatRooms", chatRooms);
+
+        return "domain/chat/chatRoom/list";
+    }
+
+    @Setter
+    @Getter
+    public static class WriteRequestBody {
+        private String writerName;
+        private String content;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class WriteResponseBody {
+        private Long chatMessageId;
+    }
+
+    @PostMapping("/{roomId}/write")
     @ResponseBody
-    public boolean makeRoom(@RequestBody ChatRoomDTO ChatRoomDTO) {
-        //ChatRoom은 ACCESS = PROTETED가 걸려있었다.
-        //외부에서 직접 생성자 호출하는 대신, 빌더 패턴이나 팩토리 메서드(?) 를 사용하는 목적이라는 듯 하다.
-        //따라서 기존의 new 방식을 주석처리하고 빌드패턴을 사용하여 작성하였다.
-        //ChatRoom chatRoom = new ChatRoom();
-        boolean successYn = true;
-        ChatRoom chatRoom = ChatRoom.builder()
-                            .name(ChatRoomDTO.getName())
-                            .build();
-        try {
-            chatRoomService.save(chatRoom);
-        } catch (Exception e) {
-            successYn = false;
-            log.error("채팅방 생성 실패", e);
-            throw new RuntimeException(e);
-        }
-        return successYn;
+    public RsData<WriteResponseBody> write(
+            @PathVariable final long roomId,
+            @RequestBody final WriteRequestBody requestBody
+    ) {
+        ChatMessage chatMessage = chatRoomService.write(roomId, requestBody.getWriterName(), requestBody.getContent());
+
+        return RsData.of("S-1", "%d번 메시지를 작성하였습니다.".formatted(chatMessage.getId()), new WriteResponseBody(chatMessage.getId()));
     }
 
-    //채팅방 생성
-/*    @PostMapping("/make")
-    public boolean makeRoom(@RequestParam ChatRoom ChatRoom) {
-        return   chatRoomService.save(ChatRoom);
-    }*/
-
-    @GetMapping("/{roomId}")
-    public String showRoom(@PathVariable long roomId, @RequestParam(defaultValue = "NoName") String writerName, Model model) {
-        model.addAttribute("roomId", roomId);
-        model.addAttribute("writerName", writerName);
-         return "domain/chat/chatRoom/make";
+    @Getter
+    @AllArgsConstructor
+    public static class GetMessagesAfterResponseBody {
+        private List<ChatMessage> messages;
     }
 
+    @GetMapping("/{roomId}/messagesAfter/{afterId}")
+    @ResponseBody
+    public RsData<GetMessagesAfterResponseBody> getMessagesAfter(
+            @PathVariable final long roomId,
+            @PathVariable final long afterId
+    ) {
+        List<ChatMessage> messages = chatMessageService.findByChatRoomIdAndIdAfter(roomId, afterId);
 
-
+        return RsData.of("S-1", "%d개의 메시지를 가져왔습니다.".formatted(messages.size()), new GetMessagesAfterResponseBody(messages));
+    } //메세지 확인후 채팅방이동
 }
